1af17cda6a91315428efb42283b22c34da277949
Juan Naranjo <juanesnc16@gmail.com>
2024-02-28 17:51:42 -0500

    Creacion de excepciones para posibles errores en la URL realizado

--- models/server.js
+++ models/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const uuidValidate = require('uuid-validate')
 
 class Server {
 
-    constructor(){
+    constructor() {
         this.app = express();
         this.port = process.env.PORT;
         this.url = '/api/sales';
@@ -14,21 +15,78 @@ class Server {
         this.routes();
     }
 
-    middlewares(){
+    middlewares() {
 
         //Cors
-        this.app.use( cors() );
+        this.app.use(cors());
         //Lectura y parseo del body
-        this.app.use( express.json() );
+        this.app.use(express.json());
+        //Validar URL
+        this.app.use(this.validarUrl.bind(this));
 
     }
 
-    routes(){
-        
+    validarUrl(req, res, next) {
+        const url = req.originalUrl;
+
+        const urlBaseRegex = /^\/api\/sales\/.*$/;
+
+        if (!url || !urlBaseRegex.test(url)) {
+            return res.status(403).json({
+                error: 'URL invalida!'
+            });
+        }
+
+        const rutaBase = '/api/sales/';
+
+        const rutaEspecifica = url.slice(rutaBase.length).split('/')[0];
+
+        const rutasPermitidas = [
+            'crearVenta',
+            'actualizarVenta',
+            'eliminarVenta',
+            'listarVentas',
+            'listarProductos',
+            'crearProductos',
+            'crearUsuario',
+            'listarUsuario',
+            'borrarUsuario',
+            'asignarRol',
+            'crearRol',
+            'cierresDiarios',
+            'cierresMensuales'
+        ];
+
+        if (!rutasPermitidas.includes(rutaEspecifica)) {
+            return res.status(404).json({
+                error: 'Ruta no encontrada!'
+            });
+        }
+
+        const idRegex = /\/([a-f0-9-]+)$/;
+        const idMatch = url.match(idRegex);
+
+        if (idMatch) {
+            const id = idMatch[1];
+            if (!uuidValidate(id)) {
+                return res.status(401).json({
+                    error: 'El ID debe ser un UUID valido!'
+                });
+            }
+        } else if (rutaEspecifica != 'listarProductos' && rutaEspecifica != 'listarVentas') {
+            return res.status(401).json({
+                error: 'Falta ingresar el ID en la ruta!'
+            });
+        }
+
+        next();
+    }
+
+    routes() {
         this.app.use(this.url, require('../routes/tienda'));
     }
 
-    listen(){
+    listen() {
         this.app.listen(this.port, () => {
             console.log('Servidor corriendo en puerto', this.port);
         });
